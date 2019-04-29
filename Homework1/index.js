542a4ac661fa3839665f1a71cd0769d11f61e773
samrah.mahmood27@yahoo.com <samrahmahmood@Samrahs-MacBook-Pro.local>
2019-04-28 19:22:26 -0500

    two or more files

---
+++ Homework1/index.js
@@ -0,0 +1,16 @@
+console.log('The new file');
+
+var s = 'a string variable';
+console.log(s);
+
+console.log(document);
+console.log($(document));
+
+//css log
+console.log($('div'));
+
+//console.log($);
+//$(document).ready(function(){
+  //  console.log('Yes');
+   
+//});
